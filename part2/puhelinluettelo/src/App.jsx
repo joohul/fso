6078b728ff03efb6a86e5a1e7f89de97fe931597
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-05 18:01:53 +0300

    Puhelinluettelo 2.10 refaktorointi komponentteihin

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,5 +1,53 @@
 import { useState } from 'react'
 
+// Show one person
+const DisplayPerson = (props) => {
+  const { person } = props
+  return (
+    <p>
+      {person.name} {person.number}
+    </p>
+  )
+}
+
+// Add a new person to the phonebook
+const PersonForm = (props) => {
+  const { addPerson, newName, handleNameChange, newNumber, handleNumberChange } = props
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+   )
+}
+
+// Search for a person in the phonebook
+const SearchForm = (props) => {
+  const { newSearch, handleSearchChange } = props
+  return (
+    <div>
+      search: <input value={newSearch} onChange={handleSearchChange} />
+    </div>
+   )
+}
+
+// Display multiple persons
+const DisplayPersons = (props) => {
+  const { persons } = props
+  return (
+    <div>
+      {persons.map(person => <DisplayPerson key={person.name} person={person} />)}
+    </div>
+   )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
@@ -36,28 +84,18 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-const handleSearchChange = (event) => {
-  setNewSearch(event.target.value)
-}
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value)
+  }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <h1>Phonebook</h1>
+      <h2>Add new person</h2>
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      search: <input value={newSearch} onChange={handleSearchChange} />
-      {/* Filter the persons based on the search and display*/}
-      {persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase())).map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      <SearchForm newSearch={newSearch} handleSearchChange={handleSearchChange} />
+      <DisplayPersons persons={persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase()))} />
     </div>
   )
 
