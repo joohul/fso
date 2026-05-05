88676a8150578cae1320162ab4eba0584e6658a9
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-05 18:17:15 +0300

    Puhelinluettelo 2.11 Alkutilan hakeminen palvelimelta

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect, use } from 'react'
+import axios from 'axios'
 
 // Show one person
 const DisplayPerson = (props) => {
@@ -49,12 +50,16 @@ const DisplayPersons = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
