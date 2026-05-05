334e0b6298261ae774e021f16470cf254a14597b
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-06 00:01:28 +0300

    3.10 ja 3.11 Front- ja backend verkkoon

--- part3/puhelinluettelo/index.js
+++ part3/puhelinluettelo/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const cors = require('cors')
 
-app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (request, response) => {
   return JSON.stringify(request.body)
