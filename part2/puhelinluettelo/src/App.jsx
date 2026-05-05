44067b227da0572e6c2c86bb153f3f4dc7c4ee8c
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-05 17:52:05 +0300

    Puhelinluettelo 2.6-2.9

---
+++ part2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    // Check if the name already exists in the phonebook
+    // Number doesn't need to be checked per the excercise (?)
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+const handleSearchChange = (event) => {
+  setNewSearch(event.target.value)
+}
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      search: <input value={newSearch} onChange={handleSearchChange} />
+      {/* Filter the persons based on the search and display*/}
+      {persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase())).map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+    </div>
+  )
+
+}
+
+export default App
