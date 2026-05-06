2271de8b0a83906fe941f4c5faadc8d2337c4b8f
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-06 22:01:19 +0300

    Middlewaren siirto omaan tiedostoon

--- part4/blogilista/app.js
+++ part4/blogilista/app.js
@@ -5,37 +5,16 @@ const jwt = require('jsonwebtoken')
 const express = require('express')
 const app = express()
 app.use(express.json())
+const middleware = require('./utils/middleware')
 
-const tokenExtractor = (request, response, next) => {
-	const authorization = request.get('authorization')
-	if (authorization && authorization.startsWith('Bearer ')) {
-		request.token = authorization.replace('Bearer ', '')
-	}
-	next()
-}
 
-const userExtractor = async (request, response, next) => {
-	if (request.token) {
-		try {
-			const decodedToken = jwt.verify(request.token, process.env.SECRET)
-			if (decodedToken.id) {
-				request.user = await User.findById(decodedToken.id)
-			}
-		} catch (error) {
-			// Can't find user for some reason.
-      request.user = null
-		}
-	}
-	next()
-}
-
-app.use(tokenExtractor)
+app.use(middleware.tokenExtractor)
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 
-app.use('/api/blogs', userExtractor, blogsRouter)
+app.use('/api/blogs', middleware.userExtractor, blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
