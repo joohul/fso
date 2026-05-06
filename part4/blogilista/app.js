c97ca5bf4160c1acf5907f2fc9bb3dfa6c7f9d52
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-06 17:46:37 +0300

    4.15 ja 4.16 Käyttäjien lisäys, rajoitteet ja testit

--- part4/blogilista/app.js
+++ part4/blogilista/app.js
@@ -5,6 +5,10 @@ const app = express()
 app.use(express.json())
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
 
 module.exports = app
