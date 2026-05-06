6466467f5fef556d6eefa9190fe6cc505f854b62
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-06 16:07:57 +0300

    4.6 ja 4.7 most... testit

--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -6,7 +6,33 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favouriteBlog = (blogs) => {
+  const likes = blogs.map(blog => blog.likes)
+  return blogs[likes.indexOf(Math.max(...likes))]
+}
+
+const mostBlogs = (blogs) => {
+  const authors = blogs.map(blog => blog.author)
+  const count = Array(authors.length).fill(0)
+  authors.forEach(author => {
+    count[authors.indexOf(author)] = (count[authors.indexOf(author)]) + 1
+  })
+  return { author: authors[count.indexOf(Math.max(...count))], blogs: Math.max(...count) }
+}
+
+const mostLikes = (blogs) => {
+  const authors = blogs.map(blog => blog.author)
+  const likes = Array(authors.length).fill(0)
+  blogs.forEach(blog => {
+    likes[authors.indexOf(blog.author)] = (likes[authors.indexOf(blog.author)]) + blog.likes
+  })
+  return { author: authors[likes.indexOf(Math.max(...likes))], likes: Math.max(...likes) }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog,
+  mostBlogs,
+  mostLikes
 }
