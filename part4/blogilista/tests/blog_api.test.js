05c39f07a89bd55b3d5d062a50b5827f77bba68f
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-06 16:39:59 +0300

    4.10 ja 4.11 Blogien lisäämisen testit sekä default-arvo likes-kentälle.

--- part4/blogilista/tests/blog_api.test.js
+++ part4/blogilista/tests/blog_api.test.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const { resolve } = require('node:dns')
 
 const api = supertest(app)
 
@@ -55,6 +56,42 @@ test('identifier field is named id', async () => {
   response.body.forEach(blog => {
     assert.ok(blog.id)
   })
+})
+
+// Test HTTP POST to /api/blogs creates a new blog post
+test('a new blog can be added', async () => {
+  const newBlog = {
+    title: "Test", 
+    author: "Author",
+    url: "http://test.com",
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+
+  // Check that number of blgs increases
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, 3)
+})
+
+// Test HTTP POST to /api/blogs without likes defaults to 0
+
+test('a new blog can be added without likes', async () => {
+  const newBlog = {
+    title: "Test", 
+    author: "Author",
+    url: "http://test.com",
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+
+  assert.strictEqual(response.body.likes, 0)
 })
 
 after(async () => {
