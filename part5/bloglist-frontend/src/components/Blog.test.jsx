625658d0f0be2f6a74b2a411cfaab9973e3d83bd
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-07 20:36:21 +0300

    5.14 Lisätietojen näytön testi

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react'
-import { test, expect } from 'vitest'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
   const blog = {
-    title: 'This is a title',
-    author: 'This is an author',
-    url: 'This is a url',
+    title: 'Title',
+    author: 'Author',
+    url: 'URL',
     likes: 0,
     user: {
       id: '12345',
@@ -16,9 +16,39 @@ test('renders content', () => {
 
   render(<Blog blog={blog} />)
 
-  const element = screen.getByText('This is a title This is an author')
+  const element = screen.getByText('Title Author')
 
   screen.debug(element)
 
   expect(element).toBeDefined()
+})
+
+
+test('clicking the button shows additional information', async () => {
+  const blog = {
+    title: 'Title',
+    author: 'Author',
+    url: 'URL',
+    likes: 0,
+    user: {
+      id: '12345',
+      name: 'Test User'
+    }
+  }
+
+  render(
+    <Blog blog={blog} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('show details')
+  await user.click(button)
+
+  const urlElement = screen.getByText('URL')
+  const likesElement = screen.getByText('0 likes')
+  const userElement = screen.getByText('Test User')
+  
+  expect(urlElement).toBeDefined()
+  expect(likesElement).toBeDefined()
+  expect(userElement).toBeDefined()
 })
