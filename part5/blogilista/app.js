7ed0ca4e34a5b2fb2ba01762000e6002c087984a
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-07 21:05:06 +0300

    5.17 Backendin reset-toiminnallisuus

--- part5/blogilista/app.js
+++ part5/blogilista/app.js
@@ -14,6 +14,11 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use('/api/blogs', middleware.userExtractor, blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
