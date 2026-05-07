3a59687b612e7b996367f9d7f9cb39f173216328
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-07 23:10:51 +0300

    5.23 Likejen mukainen järjestys -testi

--- part5/E2E-tests/tests/blog_login.spec.js
+++ part5/E2E-tests/tests/blog_login.spec.js
@@ -1,4 +1,6 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const blog = require('../../blogilista/models/blog')
+const { debug } = require('console')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -117,6 +119,43 @@ describe('Blog app', () => {
       await page.getByRole('button', { name: 'show details' }).click()
       const removeButton = page.getByRole('button', { name: 'remove' })
       await expect(removeButton).not.toBeVisible()
+    })
+    
+    test('blogs get ordered by likes', async ({ page }) => {
+      // Create two blogs
+      await page.getByRole('button', { name: 'create new blog' }).click()
+      await page.getByRole('textbox').nth(0).fill('Blog1')
+      await page.getByRole('textbox').nth(1).fill('Author')
+      await page.getByRole('textbox').nth(2).fill('url.fi')
+      await page.getByRole('button', { name: 'create' }).click()
+      await page.getByRole('button', { name: 'create new blog' }).click()
+      await page.getByRole('textbox').nth(0).fill('Blog2')
+      await page.getByRole('textbox').nth(1).fill('Author')
+      await page.getByRole('textbox').nth(2).fill('url.fi')
+      await page.getByRole('button', { name: 'create' }).click()
+
+      const blog1 = page.locator('.blog', { hasText: 'Blog1 Author' })
+      const blog2 = page.locator('.blog', { hasText: 'Blog2 Author' })
+
+      // Like Blog2 twice and verify it becomes first
+      await blog2.getByRole('button', { name: 'show details' }).click()
+      await blog2.getByRole('button', { name: 'like' }).click()
+      await expect(blog2).toContainText('1 likes')
+      await blog2.getByRole('button', { name: 'like' }).click()
+      await expect(blog2).toContainText('2 likes')
+
+      await expect(page.locator('.blog').nth(0)).toContainText('Blog2 Author')
+
+      // Like Blog1 three times and verify it becomes first again
+      await blog1.getByRole('button', { name: 'show details' }).click()
+      await blog1.getByRole('button', { name: 'like' }).click()
+      await expect(blog1).toContainText('1 likes')
+      await blog1.getByRole('button', { name: 'like' }).click()
+      await expect(blog1).toContainText('2 likes')
+      await blog1.getByRole('button', { name: 'like' }).click()
+      await expect(blog1).toContainText('3 likes')
+
+      await expect(page.locator('.blog').nth(0)).toContainText('Blog1 Author')
     })
   })
 })
