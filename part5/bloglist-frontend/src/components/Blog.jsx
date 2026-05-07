4b60436f2251a2e313b74186d81746d09e5fc07c
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-07 12:32:33 +0300

    5.7 Blogin tietojen näyttö/piilotus

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,22 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  return (
+    <div>
+      {blog.title} {blog.author}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? 'hide details' : 'show details'}
+      </button>
+      {showDetails && (
+        <div>
+          <p>{blog.url}</p>
+          <p>{blog.likes} likes</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
