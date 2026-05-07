b310713ffee2dcbd7ff5271d164a663387917693
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-07 11:05:55 +0300

    5.4 Virhe- ja onnistumisilmoitukset, refaktorointia

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -4,33 +4,12 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
-const handleLogin = (event, setUser) => {
-  event.preventDefault()
-  loginService.login({
-    username: event.target.Username.value,
-    password: event.target.Password.value
-  }).then(user => {
-    setUser(user)
-    window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
-  })
-}
+import SuccessNotification from './components/SuccessNotification'
+import ErrorNotification from './components/ErrorNotification'
 
-const handleNewBlog = (event, user, blogs, setBlogs, newTitle, setNewTitle, newAuthor, setNewAuthor, newUrl, setNewUrl) => {
-  event.preventDefault()
-  const newBlog = {
-    title: newTitle,
-    author: newAuthor,
-    url: newUrl
-  }
-  blogService.create(newBlog, user.token).then(createdBlog => {
-    setBlogs(blogs.concat(createdBlog))
-    setNewTitle('')
-    setNewAuthor('')
-    setNewUrl('')
-  })
-}
 
-const LoginForm = ({ onLogin }) => {
+const LoginForm = (props) => {
+  const {onLogin, handleLogin} = props
   return (
     <div>
       <h2>log in to application</h2>
@@ -48,7 +27,7 @@ const LoginForm = ({ onLogin }) => {
 }
 
 const NewBlogForm = (props) => {
-  const { user, blogs, setBlogs, newTitle, setNewTitle, newAuthor, setNewAuthor, newUrl, setNewUrl } = props
+  const { handleNewBlog, user, blogs, setBlogs, newTitle, setNewTitle, newAuthor, setNewAuthor, newUrl, setNewUrl } = props
   return (
     <div>
       <h2>create new</h2>
@@ -74,6 +53,8 @@ const App = () => {
   const [newTitle, setNewTitle] = useState('')
   const [newAuthor, setNewAuthor] = useState('')
   const [newUrl, setNewUrl] = useState('')
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -90,21 +71,74 @@ const App = () => {
     }
   }, [])
 
+    const showError = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
+
+  const showSuccess = (message) => {
+    setSuccessMessage(message)
+    setTimeout(() => {
+      setSuccessMessage(null)
+    }, 5000)
+  }
+
+  const handleLogin = (event, setUser) => {
+    event.preventDefault()
+    loginService.login({
+      username: event.target.Username.value,
+      password: event.target.Password.value
+    }).then(user => {
+      setUser(user)
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
+      showSuccess('login successful')
+    }).catch(() => {
+      showError('wrong username or password')
+    })
+  }
+
+const handleNewBlog = (event, user, blogs, setBlogs, newTitle, setNewTitle, newAuthor, setNewAuthor, newUrl, setNewUrl) => {
+  event.preventDefault()
+  const newBlog = {
+    title: newTitle,
+    author: newAuthor,
+    url: newUrl
+  }
+  blogService.create(newBlog, user.token).then(createdBlog => {
+    setBlogs(blogs.concat(createdBlog))
+    setNewTitle('')
+    setNewAuthor('')
+    setNewUrl('')
+    showSuccess(`a new blog ${createdBlog.title} by ${createdBlog.author} added`)
+  })
+  }
+
   if (!user) {
-    return <LoginForm onLogin={setUser} />
+    return (
+    <div>
+        <SuccessNotification message={successMessage} />
+        <ErrorNotification message={errorMessage} />
+      <LoginForm onLogin={setUser} handleLogin={handleLogin} />
+    </div>
+    )
   }
   else{
     return (
       <div>
+        <SuccessNotification message={successMessage} />
+        <ErrorNotification message={errorMessage} />
         <h2>blogs</h2>
         <p></p>
         <p>{user.name} is logged in</p>
         <button onClick={() => {
           setUser(null)
           window.localStorage.removeItem('loggedBlogAppUser')
+          showSuccess('logged out')
         }}>logout</button>
         <p></p>
-        <NewBlogForm user={user} blogs={blogs} setBlogs={setBlogs} newTitle={newTitle} setNewTitle={setNewTitle} newAuthor={newAuthor} setNewAuthor={setNewAuthor} newUrl={newUrl} setNewUrl={setNewUrl} />
+        <NewBlogForm handleNewBlog={handleNewBlog} user={user} blogs={blogs} setBlogs={setBlogs} newTitle={newTitle} setNewTitle={setNewTitle} newAuthor={newAuthor} setNewAuthor={setNewAuthor} newUrl={newUrl} setNewUrl={setNewUrl} />
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
