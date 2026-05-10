81dacb3039f868adfed2400b13d3446637a62dc8
Joona Hulkkonen (TAU) <joona.e.hulkkonen@tuni.fi>
2026-05-10 11:06:18 +0300

    6.16 Haku Queryllä

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,18 +1,45 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
+import { useQuery } from '@tanstack/react-query'
+
 const App = () => {
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
+  /*
   const anecdotes = [
     {
       content: 'If it hurts, do it more often',
       id: '47145',
       votes: 0,
     },
-  ]
+  ]*/
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    retry: false,
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3001/anecdotes')
+      if (!response.ok) {
+        throw new Error('Failed to fetch')
+      }
+      return await response.json()
+    }
+  })
+ 
+  console.log(JSON.parse(JSON.stringify(result)))
+ 
+  if (result.isPending) {
+    return <div>loading data...</div>
+  }
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  } 
+
+  const anecdotes = result.data
+
 
   return (
     <div>
