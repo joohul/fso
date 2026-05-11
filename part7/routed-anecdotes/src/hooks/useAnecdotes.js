4e38a6de581e0d900f19873ad0884d13718218cf
dsjohu <joona.e.hulkkonen@tuni.fi>
2026-05-11 14:51:30 +0300

    7.4 ja 7.5 useAnecdotes-hook

---
+++ part7/routed-anecdotes/src/hooks/useAnecdotes.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import anecdoteService from '../services/anecdotes'
+
+export const useAnecdotes = () => {
+  const [anecdotes, setAnecdotes] = useState([])
+
+  useEffect(() => {
+    anecdoteService.getAll().then(data => setAnecdotes(data))
+  }, [])
+
+  const addAnecdote = (anecdote) => {
+    anecdoteService.createNew(anecdote).then(newAnecdote => {
+      setAnecdotes(anecdotes.concat(newAnecdote))
+    })
+  }
+
+  return { anecdotes, addAnecdote }
+}
